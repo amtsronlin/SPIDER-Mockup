05854ba638e0d5471c2c5ac7730dc5bdbe09d497
amtsronlin <nhamchithanh@gmail.com>
2018-04-24 18:13:56 +0700

    Update wisearch.js

--- wisearch.js
+++ wisearch.js
@@ -1,49 +1,55 @@
 var index = 2;
 var j = 1;
 var clickedElement;
-var status = '<select class="selectpicker" id="status" multiple data-live-search="true">'+
-'<option value="track">Accepted</option>'+
-'<option value="parent">Approved</option>'+
-'<option value="relate">Closed</option>'+
-'<option value="implement">In progress</option>'+
-'<option value="child">New</option>'+
-'</select>';
-var create = '<select class="selectpicker" id="create" data-live-search="true">'+
-'<option value="track">Mary</option>'+
-'<option value="parent">Peter</option>'+
-'<option value="relate">Join</option>'+
-'<option value="implement">Tom</option>'+
-'<option value="child">Laura</option>'+
-'</select>';
-var create = '<select class="selectpicker" id="create" data-live-search="true">'+
-'<option value="track">Mary</option>'+
-'<option value="parent">Peter</option>'+
-'<option value="relate">Join</option>'+
-'<option value="implement">Tom</option>'+
-'<option value="child">Laura</option>'+
-'</select>';
-var own = '<select class="selectpicker" id="own" data-live-search="true">' +
-'<option value="track">Mary</option>' +
-'<option value="parent">Peter</option>' +
-'<option value="relate">Join</option>' +
-'<option value="implement">Tom</option>' +
-'<option value="child">Laura</option>' +
-'</select>';
-var file = '<select class="selectpicker" id="file" multiple data-live-search="true">' +
-'<option value="track">RBEI</option>' +
-'<option value="parent">MCP</option>' +
-'<option value="relate">RCAR</option>' +
-'<option value="implement">V850</option>' +
-'<option value="child">TC3X</option>' +
-'</select>'
-var due = '<input type="text" style="display:none;" id="due" name="due">'
-$(document).ready(function () {
+var status = 'Status:<select class="selectpicker" id="status" multiple data-live-search="true">' +
+    '<option value="track">Accepted</option>' +
+    '<option value="parent">Approved</option>' +
+    '<option value="relate">Closed</option>' +
+    '<option value="implement">In progress</option>' +
+    '<option value="child">New</option>' +
+    '</select><div class="remove"><i class="fa fa-trash" aria-hidden="true"></i></div>';
+var create = 'Create:<select class="selectpicker" id="create" data-live-search="true">' +
+    '<option value="track">Mary</option>' +
+    '<option value="parent">Peter</option>' +
+    '<option value="relate">Join</option>' +
+    '<option value="implement">Tom</option>' +
+    '<option value="child">Laura</option>' +
+    '</select><div class="remove"><i class="fa fa-trash" aria-hidden="true"></i></div>';
+var own = 'Own:<select class="selectpicker" id="own" data-live-search="true">' +
+    '<option value="track">Mary</option>' +
+    '<option value="parent">Peter</option>' +
+    '<option value="relate">Join</option>' +
+    '<option value="implement">Tom</option>' +
+    '<option value="child">Laura</option>' +
+    '</select><div class="remove"><i class="fa fa-trash" aria-hidden="true"></i></div>';
+var file = 'File:<select class="selectpicker" id="file" multiple data-live-search="true">' +
+    '<option value="track">RBEI</option>' +
+    '<option value="parent">MCP</option>' +
+    '<option value="relate">RCAR</option>' +
+    '<option value="implement">V850</option>' +
+    '<option value="child">TC3X</option>' +
+    '</select><div class="remove"><i class="fa fa-trash" aria-hidden="true"></i></div>'
+var due = 'Due:<input type="text" id="due" name="due"><span class="glyphicon glyphicon-calendar"></span><div class="remove"><i class="fa fa-trash" aria-hidden="true"></i></div>'
+function GetURLParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}
+var rtc = GetURLParameter('rtc');
 
-    // $(".path-right" + (i -1)).click(function(){
-    //     $('#node' + i + "-dom").html("<div class='node-display' id='node" + i +"-display' title='Work Item'></div><div class='path path-right'></div>");
-    //     i++;
-    //     $('.query-diagram-area').append('<div id="node' + i + '-dom" class="resource-node group-object"></div>');         
-    // });
+ $(document).ready(function () {
+
+    if (rtc === "1") {
+        console.log(rtc);
+        $(".conditionTab2").remove();
+    } else {
+        $(".conditionTab1").remove();
+    }
 
     $("#delete_table").click(function () {
         if (i > 1) {
@@ -63,7 +69,7 @@ $(document).ready(function () {
         var select = $("select", this).val();
         var radio = $("input[name=optradio]:checked", this).val();
         var idx = $("input[name=index]", this).val();
-        if (select !== null) {
+        if (select != '') {
             $(".djs-label-d", clickedElement).html('<tspan x="6" y="62">' + select + '</tspan>');
             $(".djs-label-u", clickedElement).html('<tspan x="6" y="45">' + radio + '</tspan>');
             addNode(idx, select, this);
@@ -86,10 +92,15 @@ $(document).ready(function () {
         });
     });
 
+    $(document).on('click', ".remove", function (e) {
+        alert($(this).parent());
+        $(this).parent().remove();
+    });
+
     function addNode(i, select, event) {
         if (index == i) {
             index++;
-            $('#node' + i + "-dom").html("<div class='node-display' id='node" + i + "-display' title='Work Item' class='context' data-toggle='context' data-target='#context-menu'><input type='text'/></div><div class='path path-right' data-index=" + index + " data-toggle='modal' data-target='#myModal'>" + 
+            $('#node' + i + "-dom").html("<div class='node-display' id='node" + i + "-display' title='Work Item' class='context' data-toggle='context' data-target='#context-menu'><input class='wiInput' type='text' value='wi"+i+"'/></div><div class='path path-right' data-index=" + index + " data-toggle='modal' data-target='#myModal'>" +
                 "<svg width='90px' height='100px'><g><defs><marker id='marker' viewBox='0 0 20 20' refX='11' refY='10' markerWidth='10' markerHeight='10' orient='auto'><path d='M 1 5 L 11 10 L 1 15 Z' style='fill: black; stroke-width: 1px; stroke-linecap: round; stroke-dasharray: 10000, 1; stroke: black;'></path>" +
                 "</marker></defs ><text class='djs-label-u' style='font-family: Arial, sans-serif; font-size: 11px;'></text><line x1='0' y1='50' x2='90' y2='50' marker-end='url(#marker)' stroke='black' stroke-width='2' /><text class='djs-label-d' style='font-family: Arial, sans-serif; font-size: 11px;'></text>" +
                 "</g ></svg ></div>");
@@ -100,40 +111,52 @@ $(document).ready(function () {
 
     $('#wiModal .save').click(function (e) {
         e.preventDefault();
-        addImage(5);
-        $('#myModal').modal('hide');
+        $('#wiModal').modal('hide');
         //$(this).tab('show')
         return false;
     })
 
+     $('.choose').click(function (e) {
+         e.preventDefault();
+         $('#wiModal').modal('hide');
+         //$(this).tab('show')
+         return false;
+     })
+
     $('#wiModal').on('hidden.bs.modal', function () {
         $('#popupRC').css({ display: 'none' });
     });
-    $('#wiModal').on('show.bs.modal', function () {
-        $('#status').css({ display: 'none' });
-        $('#create').css({ display: 'none' });
-        $('#own').css({ display: 'none' });
-        $('#file').css({ display: 'none' });
-    });
 
     function addAtt(htmlTxt) {
-        $('add-attr' + j).html(htmlTxt);
+        $('#add-attr' + j).html(htmlTxt);
         j++;
-        $('.query-diagram-area').append('<div id="add-attr'+j +'"></div>');
-
+        $('.d-flex').append('<div class="add-attr" id="add-attr' + j + '"></div>');
     }
 
-
     $('#condSelect').on('changed.bs.select', function (e) {
-        addAtt($('#' + $('#condSelect').selectpicker('val')))
+        if ($('#condSelect').selectpicker('val') === 'due') {
+            addAtt(due);
+            $("#due").datepicker();
+        } else if ($('#condSelect').selectpicker('val') == 'own') {
+            addAtt(own);
+        } else if ($('#condSelect').selectpicker('val') == 'create') {
+            addAtt(create);
+        } else if ($('#condSelect').selectpicker('val') == 'status') {
+            addAtt(status);
+        } else if ($('#condSelect').selectpicker('val') == 'file') {
+            addAtt(file);
+        }
+        $(".selectpicker").selectpicker('refresh');
     });
 
+ 
 
 })
 
-function remove() {
+function remove(e) {
     $(".djs-label-d", clickedElement).html('');
     $(".djs-label-u", clickedElement).html('');
-    var select = $("select", this).val("");
-    var radio = $("input[name=optradio]", this).val("");
+    var select = $("#myModal select").val("");
+    var radio = $("#myModal input[name=optradio]").val("");
 }
+
